8e90ec568df382eb43fe691d5d8d957d69f021e6
Br4nd0n1 <142539603+Br4nd0n1@users.noreply.github.com>
2023-12-16 09:20:45 -0800

    This is a demo of some tailwind components (#4)
    Added some demo components using tailwind. I chose to us tailwind
    because we are using the app router. with the app router not all ui
    librarys such as chakra are supported. as such the hope was that
    building the components using tailwind we provide use with better
    compatibility.

---
+++ src/app/components/badge_tw.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface props {
+  variant: string;
+  className: string;
+  children: ReactNode;
+}
+
+export default function Badge_TW({variant, className, children}: props) {
+    return (
+      <>
+        <span className={`inline-flex items-center rounded-md ${variant} ${className} px-2 py-1 text-xs font-medium ring-1 ring-inset ring-gray-500/10`}>{children}</span>
+      </>
+    )
+  }
---
+++ src/app/testing/page.tsx
@@ -0,0 +1,109 @@
+'use client'
+import React from 'react'
+import Button_TW from '../components/button_tw'
+import Badge_TW from '../components/badge_tw'
+import Dropdown_TW from '../components/dropdown_tw'
+import Navbar_TW from '../components/navbar_tw'
+import Modal_TW from '../components/modal_tw'
+
+export default function Test() {
+  const categories: string[] = [
+    'primary',
+    'secondary',
+    'success',
+    'warning',
+    'danger',
+    'info',
+    'light',
+    'dark',
+  ]
+  const shades: string[] = [
+    'bg-indigo-300',
+    'bg-indigo-400',
+    'bg-indigo-500',
+    'bg-indigo-600',
+    'bg-indigo-700',
+    'bg-indigo-800',
+    'bg-indigo-900',
+  ]
+
+  const [colors, setColors] = React.useState<string[]>(categories)
+
+  function setNewColors(newColors: string[]) {
+    console.log('The colors are being changed')
+    setColors(newColors)
+  }
+
+  return (
+    <>
+      <Navbar_TW></Navbar_TW>
+      <Button_TW
+        variant="secondary"
+        className={`text-white mx-2 my-5`}
+        onClick={() => setNewColors(categories)}
+      >
+        Category
+      </Button_TW>
+      <Button_TW
+        variant="secondary"
+        className={`text-white mx-2 my-5`}
+        onClick={() => setNewColors(shades)}
+      >
+        Shade
+      </Button_TW>
+      <h1 className="m-2 text-2xl">Buttons</h1>
+      <br></br>
+      <div className="flex mt-2">
+        {colors.map((color, index) => (
+          <Button_TW
+            key={index + 'button'}
+            variant={color}
+            className={`text-white mx-2`}
+            onClick={() => {}}
+          >
+            {color.charAt(0).toUpperCase() + color.slice(1)}
+          </Button_TW>
+        ))}
+      </div>
+
+      <h1 className="ms-2 mt-5 text-2xl">Badges</h1>
+      <br></br>
+      <div className="flex">
+        {colors.map((color, index) => (
+          <Badge_TW
+            key={index + 'badge'}
+            variant={color}
+            className={`text-white mx-2`}
+          >
+            {color.charAt(0).toUpperCase() + color.slice(1)}
+          </Badge_TW>
+        ))}
+      </div>
+
+      <h1 className="ms-2 mt-5 text-2xl">Text</h1>
+      <br></br>
+      <div className="flex">
+        {colors.map((color, index) => (
+          <p
+            key={index + 'p'}
+            className={`text-${color.replace('bg-', '')} mx-2`}
+          >
+            {color.charAt(0).toUpperCase() + color.slice(1)}
+          </p>
+        ))}
+      </div>
+      <h1 className="ms-2 mt-5 text-2xl">Dropwdown</h1>
+      <br></br>
+      <div className="w-40 ms-2">
+        <Dropdown_TW variant="secondary" className="text-white">
+          Options
+        </Dropdown_TW>
+      </div>
+      <h1 className="ms-2 mt-5 text-2xl">Modal</h1>
+      <br></br>
+      <Modal_TW variant="secondary" className="ms-2 text-white">
+        Open
+      </Modal_TW>
+    </>
+  )
+}
